0a048b975d6b5048cc514adfc97f8789a1fcaff0
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2024-02-02 13:18:15 +0500

    - все макеты готовы - начало написание логики

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,10 +36,18 @@ android {
 }
 
 dependencies {
+    implementation(project(":core"))
+    implementation(project(":account_profile_screen"))
+    implementation(project(":auth_screen"))
+    implementation(project(":catalogue_screen"))
+    implementation(project(":favourite_screen"))
+    implementation(project(":product_card_screen"))
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 
 }
--- favourite_screen/build.gradle.kts
+++ favourite_screen/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.test.application.core"
+    namespace = "com.test.application.favorite_screen"
     compileSdk = 34
 
     defaultConfig {
@@ -37,10 +37,8 @@ android {
 
 dependencies {
 
+    implementation(project(":core"))
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
